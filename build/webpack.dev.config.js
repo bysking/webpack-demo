87c1212b23cbaefd82630d7ae0049e8a7264c5e4
chenpengxu <chenpengxu@dding.cn>
2019-09-29 15:28:54 +0800

    feat: html-webpack-plugin插件自动根据模板生成index.html 同时注入打包后的bundle.js

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,9 +1,20 @@
 // 只在开发模式使用的打包配置文件
 const merge = require('webpack-merge') // npm install webpack-merge --save-dev
 const webpackBaseConfig = require('./webpack.base.config')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require('path')
+
 module.exports = merge(webpackBaseConfig, {
   mode: "development", // 指定模式，不要压缩，丑化
   output: {
     filename: '[name].js'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'bysking-webpack-demo',
+      filename: '../index.html',
+      template: path.resolve(__dirname, './config/index.html'),
+      injetc: false
+    })
+  ]
 })
