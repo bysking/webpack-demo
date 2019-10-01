4b74ec58193262962d75ec2cfdea95116ef3659a
chenpengxu <chenpengxu@dding.cn>
2019-10-01 23:07:43 +0800

    feat: 开发环境出口配置

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,1 +1,14 @@
 //  只在生产环境的打包配置文件
+
+const baseConfig = require('./webpack.base.config')
+const merge = require('webpack-merge')
+const path = require('path')
+
+module.exports = merge(baseConfig, {
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, '../widget/code/dist/'),
+    publicPath: './dist',
+    filename: '[name].[hash].js'
+  }
+})
